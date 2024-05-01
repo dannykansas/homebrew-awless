7223cfc7d48ba22d70bfb765deb22a4652d082f9
Danny Fowler <danny.fowler@nextroll.com>
2024-05-01 10:34:49 -0700

    Adding formula to main branch

---
+++ Formula/awless.rb
@@ -0,0 +1,54 @@
+class Awless < Formula
+  version = "v0.1.11"
+
+  desc "The Mighty CLI for AWS"
+  homepage "https://github.com/dannykansas/awless"
+  url "https://github.com/dannykansas/awless.git",
+        :tag => version
+  head "https://github.com/dannykansas/awless.git"
+
+  bottle do
+    root_url "https://github.com/dannykansas/homebrew-awless/releases/download/#{version}"
+    cellar :any_skip_relocation
+    sha256 "dbd1e5f4afd1de6fe00372bd8b6fffa9a448bbf461019094a5061b1b96924096" => :sierra
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
+    dir = buildpath/"src/github.com/dannykansas/awless"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      # Make binary
+      system "go", "run", "release.go", "-tag", "v#{version}", "-brew", "-arch", "#{arch}", "-os", "darwin"
+      bin.install "awless"
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/awless completion bash")
+      (bash_completion/"awless").write output
+
+      # Install zsh completion
+      output = Utils.popen_read("#{bin}/awless completion zsh")
+      (zsh_completion/"_awless").write output
+    end
+  end
+
+  def caveats
+    <<~EOS
+
+      In order to get awless completion,
+        [bash] you need to install `bash-completion` with brew.
+        OR
+        [zsh], add the following line to your ~/.zshrc:
+          source #{HOMEBREW_PREFIX}/share/zsh/site-functions/_awless
+    EOS
+  end
+
+  test do
+    run_output = shell_output("#{bin}/awless --help 2>&1")
+    assert_match "Awless is a powerful command line tool to inspect, sync and manage your infrastructure", run_output
+  end
+end
